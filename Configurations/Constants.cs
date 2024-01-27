6d34c451c40d722fdb2a6909aa9b0a49084818df
makhammadsoliyev <makhammadsoliyev@gmail.com>
2024-01-27 13:15:33 +0500

    Add project files.

---
+++ Configurations/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace FoodWasteReductionAppForRestaurants.Configurations;
+
+public static class Constants
+{
+    public const string FOODS_PATH = @"..\..\..\..\FoodWasteReductionAppForRestaurants\DataBase\foods.json";
+    public const string SHELTERS_PATH = @"..\..\..\..\FoodWasteReductionAppForRestaurants\DataBase\shelters.json";
+    public const string DONATIONS_PATH = @"..\..\..\..\FoodWasteReductionAppForRestaurants\DataBase\donations.json";
+    public const string RESTAURANTS_PATH = @"..\..\..\..\FoodWasteReductionAppForRestaurants\DataBase\restaurants.json";
+}
